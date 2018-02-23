17a7d07e7301f738cb47260992c1ea40838cb8ef
coolreader18 <33094578+coolreader18@users.noreply.github.com>
2018-02-23 16:26:01 -0600

    indent

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 /* @legume
- * @name script
- * @script npm:jquery
- * @style style.css
- */
+* @name script
+* @script npm:jquery
+* @style style.css
+*/
 var $ = require("jquery");
 $("#input").on("input", gen)
 function gen() {
-	var arr = Array.from($("#input").val().replace(/\s/g, "_").toLowerCase());
-	for (var i = switched ? 0 : 1; i < arr.length; i+=2) {
-		arr[i] = arr[i].toUpperCase();
-	}
-	$("#output").text("xX_" + arr.join("") + "_Xx");
+  var arr = Array.from($("#input").val().replace(/\s/g, "_").toLowerCase());
+  for (var i = switched ? 0 : 1; i < arr.length; i+=2) {
+    arr[i] = arr[i].toUpperCase();
+  }
+  $("#output").text("xX_" + arr.join("") + "_Xx");
 }
 $("#switch").change(function() {
-	switched = this.checked;
-	gen();
+  switched = this.checked;
+  gen();
 })
